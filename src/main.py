9b40e80a190a1b70264fd100bbdd91edccd908f4
Jim Rhotert <rhotert@stud.uni-hannover.de>
2022-01-29 13:33:19 +0100

    more nodes to explain

--- src/main.py
+++ src/main.py
@@ -1,4 +1,3 @@
-from sqlalchemy import false
 from models import get_cora_model
 from dig.xgraph.method import SubgraphX, PGExplainer
 from dig.xgraph.method.subgraphx import find_closest_node_result
@@ -12,61 +11,60 @@ from sgx_utils import subgraph_by_node_removal
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # 55 long
 # 20 short
-node_idx = 11
+node_idxs = [11, 15, 25, 5]
 model, dataset = get_cora_model("karate")
 data = dataset[0]
-logits = model(data.x, data.edge_index)
-prediction1 = torch.exp(logits[node_idx])
-print("Prediction:", prediction1)
 
-fids = []
-for _ in range(5):
-    result = subgraphx(data.x, data.edge_index, model, node_idx=node_idx, M=40, Nmin=8, L=2)
-    nodes, edges = data.x.clone().numpy(), data.edge_index.clone()
-    for node_to_remove in result:
-        nodes, edges = subgraph_by_node_removal(nodes, edges, node_to_remove)
-    logits = model(torch.tensor(nodes), edges)
-    prediction2 = torch.exp(logits[node_idx])
-    print("Prediction", prediction2)
 
-    #c = prediction1.argmax(-1)
-    fidelity = prediction1 - prediction2
-    print("Fidelity", fidelity)
-    fids.append(fidelity)
+for node_idx in node_idxs:
+    print("NODE", node_idx)
+    print("GT", data.y[node_idx])
+    logits = model(data.x, data.edge_index)
+    prediction1 = torch.exp(logits[node_idx])
+    print("Model prediction all features:", prediction1)
+    fids = []
+    for _ in range(5):
+        result = subgraphx(data.x, data.edge_index, model, node_idx=node_idx, M=40, Nmin=8, L=2)
+        nodes, edges = data.x.clone().numpy(), data.edge_index.clone()
+        for node_to_remove in result:
+            nodes, edges = subgraph_by_node_removal(nodes, edges, node_to_remove)
+        logits = model(torch.tensor(nodes), edges)
+        prediction2 = torch.exp(logits[node_idx])
+        print("Model prediction without sgx subgraph", prediction2)
 
-# print("Fidelity mean:", sum(fids) / len(fids))
-print(fids)
-print("Fidelity subgraphx =" ,torch.stack(fids).mean(dim=0))
+        #c = prediction1.argmax(-1)
+        fidelity = prediction1 - prediction2
+        print("Fidelity", fidelity)
+        fids.append(fidelity)
 
-### explain with pyg
-explainer = GNNExplainer(model, epochs=200, return_type='log_prob')
+    print("Fidelity subgraphx =" ,torch.stack(fids).mean(dim=0))
 
-gnn_fids = []
-for _ in range(5):
-    node_feat_mask, edge_mask = explainer.explain_node(node_idx, data.x, data.edge_index)
-    print(data.edge_index.shape)
-    masked_x = data.x.clone()
-    for i, f in enumerate(node_feat_mask):
-        if f > 0.5: # above 0.5 important
-            masked_x[:, i] = torch.zeros_like(data.x[:, i])
+    ### explain with pyg
+    explainer = GNNExplainer(model, epochs=200, return_type='log_prob')
 
-    masked_edges = []
-    edge_mask = edge_mask.reshape(1, -1).T
+    gnn_fids = []
+    for _ in range(5):
+        node_feat_mask, edge_mask = explainer.explain_node(node_idx, data.x, data.edge_index)
+        masked_x = data.x.clone()
+        for i, f in enumerate(node_feat_mask):
+            if f > 0.5: # above 0.5 important
+                masked_x[:, i] = torch.zeros_like(data.x[:, i])
 
-    for i, e in enumerate(edge_mask):
-        if e.item() < 0.5: # above 0.5 important
-            masked_edges.append(data.edge_index.T[i])
+        masked_edges = []
+        edge_mask = edge_mask.reshape(1, -1).T
 
-    masked_edges = torch.stack(masked_edges).T
-    print(masked_edges.shape)
-    logits = model(masked_x, masked_edges)
-    prediction3 = torch.exp(logits[node_idx])
-    fidelity = prediction1 - prediction3
-    print("Fidelity", fidelity)
-    gnn_fids.append(fidelity)
+        for i, e in enumerate(edge_mask):
+            if e.item() < 0.5: # above 0.5 important
+                masked_edges.append(data.edge_index.T[i])
 
-print(gnn_fids)
-print("Fidelity gnn =", torch.stack(gnn_fids).mean(dim=0))
+        masked_edges = torch.stack(masked_edges).T
+        logits = model(masked_x, masked_edges)
+        prediction3 = torch.exp(logits[node_idx])
+        fidelity = prediction1 - prediction3
+        print("Fidelity", fidelity)
+        gnn_fids.append(fidelity)
+
+    print("Fidelity gnn =", torch.stack(gnn_fids).mean(dim=0))
 
 
 # # Explain with subgraphx library
