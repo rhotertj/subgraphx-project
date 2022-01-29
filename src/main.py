67017dd553d4beefd6fcadd8b3b05c118d727be5
Jim Rhotert <rhotert@stud.uni-hannover.de>
2022-01-29 15:38:53 +0100

    cleanup Co-authored-by: Julian Bilsky <julianbil@users.noreply.github.com>

--- src/main.py
+++ src/main.py
@@ -9,8 +9,7 @@ from sgx_utils import subgraph_by_node_removal
 
 # Fidelity: Remove important features and expect large change in prediction
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-# 55 long
-# 20 short
+
 node_idxs = [11, 15, 25, 5]
 model, dataset = get_cora_model("karate")
 data = dataset[0]
@@ -18,7 +17,7 @@ data = dataset[0]
 
 for node_idx in node_idxs:
     print("NODE", node_idx)
-    print("GT", data.y[node_idx])
+    print("LABEL", data.y[node_idx])
     logits = model(data.x, data.edge_index)
     prediction1 = torch.exp(logits[node_idx])
     print("Model prediction all features:", prediction1)
@@ -30,11 +29,9 @@ for node_idx in node_idxs:
             nodes, edges = subgraph_by_node_removal(nodes, edges, node_to_remove)
         logits = model(torch.tensor(nodes), edges)
         prediction2 = torch.exp(logits[node_idx])
-        print("Model prediction without sgx subgraph", prediction2)
 
         #c = prediction1.argmax(-1)
         fidelity = prediction1 - prediction2
-        print("Fidelity", fidelity)
         fids.append(fidelity)
 
     print("Fidelity subgraphx =" ,torch.stack(fids).mean(dim=0))
@@ -61,16 +58,7 @@ for node_idx in node_idxs:
         logits = model(masked_x, masked_edges)
         prediction3 = torch.exp(logits[node_idx])
         fidelity = prediction1 - prediction3
-        print("Fidelity", fidelity)
         gnn_fids.append(fidelity)
 
     print("Fidelity gnn =", torch.stack(gnn_fids).mean(dim=0))
 
-
-# # Explain with subgraphx library
-# explainer = PGExplainer(model, dataset.num_node_features, device, False)
-# result = explainer(data.x, data.edge_index)
-# explainer = SubgraphX(model, num_classes=dataset.num_classes, device=device, explain_graph=False, reward_method='mc_shapley')
-# _, explanation_results, related_preds = explainer(data.x, data.edge_index, node_idx=int(node_idx), max_nodes=8)
-# # result = find_closest_node_result(explanation_results[prediction], max_nodes=8)
-# print(explanation_results)
